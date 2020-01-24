1468f53251fba58592a2048c5b0d3c8b85956056
raushan <raushan.fet@gmail.com>
2020-01-25 01:12:07 +0530

    preview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import {Switch ,  Route} from 'react-router-dom';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage.component';
+import HatsPage from './pages/hating';
+
+import ShopPage from './pages/shop/shop.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
+      
     </div>
+      
   );
 }
 
---
+++ src/pages/hating.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+const HatsPage = () => (
+    <div>
+        <h1>I am hats...... hehe..i am coding like champ....</h1>
+    </div>
+);
+
+
+
+export default HatsPage;
